2c044e15bfaf5ef9a164031d859535fb126c349b
ShayanJamil519 <shayanjamil500@gmail.com>
2023-10-04 10:19:03 +0500

    Fonts Loaded

--- src/Components/Activities/Activities.jsx
+++ src/Components/Activities/Activities.jsx
@@ -1,5 +1,6 @@
 import "./Activities.css";
 import ActivityHeader from "./ActivityHeader/ActivityHeader";
+import ActivityStatus from "./ActivityStatus/ActivityStatus";
 
 const Activities = () => {
   return (
@@ -7,7 +8,9 @@ const Activities = () => {
       <ActivityHeader />
 
       {/* Main Container */}
-      <div className="activities__main"></div>
+      <div className="activities__main">
+        <ActivityStatus title="ABC started the collab" date="Aug 24, 8.04 PM" />
+      </div>
     </div>
   );
 };
--- src/Components/Activities/ActivityStatus/ActivityStatus.jsx
+++ src/Components/Activities/ActivityStatus/ActivityStatus.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import "./ActivityStatus.css";
 
-const ActivityStatus = () => {
-  return <div>ActivityStatus</div>;
+const ActivityStatus = ({ title, date }) => {
+  return (
+    <div className="activity__status__container">
+      <div className="title">
+        <p>{title}</p>
+      </div>
+      <p className="date">{date}</p>
+    </div>
+  );
 };
 
 export default ActivityStatus;
