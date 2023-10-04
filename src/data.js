4d5606f6911d1c24906a06bcbbbe88cf8425849c
Rizwan Ahmed <rizwanahmed060602@gmail.com>
2023-10-04 14:58:59 +0500

    My part completed

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const activitiesData = [
+    {
+        'title': 'Title',
+        'content': 'You Hear Me'
+    },
+    {
+        'title': 'Collab By',
+        'content': 'Sean Kim'
+    },
+    {
+        'title': 'Duration',
+        'content': '12 Days : 4 Hours'
+    },
+];
