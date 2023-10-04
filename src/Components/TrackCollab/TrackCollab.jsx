bb23ba5c9a02f59fd78e5f7322af93adab084fd8
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-10-04 16:01:21 +0500

    some changes

--- src/Components/TrackCollab/TrackCollab.jsx
+++ src/Components/TrackCollab/TrackCollab.jsx
@@ -1,28 +1,60 @@
 import React from "react";
-import './TrackCollab.css';
+import "./TrackCollab.css";
 import { TiTick } from "react-icons/ti";
 
 const TrackCollab = () => {
-  const options = ['Collab Started', 'Requirement Submitted', 'Collab in progress', 'Review collab', 'Complete collab']
+  const options = [
+    "Collab Started",
+    "Requirement Submitted",
+    "Collab in progress",
+    "Review collab",
+    "Complete collab",
+  ];
 
-  return <div className="track__container">
-    <h2 className="track__heading">Track Collab</h2>
-    <div className="track-section">
-      {options.map((option, index) => (
-        <div key={index} className="track-option-container">
-          <div className="track-option">
-            <div className={`track-status ${index < 3 ? 'active' : 'inactive'}`} style={{backgroungColor: `{index < 3 ? #fff : #000}` }}>
-              {index < 3 ? <TiTick /> : ''}
+  return (
+    <div className="track__container">
+      <h2 className="track__heading">Track Collab</h2>
+      <div className="track-section">
+        {options.map((option, index) => (
+          <div key={index} className="track-option-container">
+            <div className="track-option">
+              <div
+                className={`track-status ${index < 3 ? "active" : "inactive"}`}
+              >
+                {/* {index < 3 ? <TiTick /> : ""} */}
+                {index < 3 ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="9"
+                    viewBox="0 0 12 9"
+                    fill="none"
+                  >
+                    <path
+                      d="M1 4.33333L4.33333 7.66667L11 1"
+                      stroke="black"
+                      stroke-opacity="0.95"
+                      stroke-width="1.8"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                ) : (
+                  " "
+                )}
+              </div>
+              <p className="track__option">{option}</p>
             </div>
-            <p className="track__option">{option}</p>
+            {index < options.length - 1 && (
+              <div
+                className={`line ${index < 3 ? "active" : "inactive"}`}
+              ></div>
+            )}
           </div>
-          {index < options.length - 1 && (
-            <div className={`line ${index < 3 ? 'active' : 'inactive'}`}></div>
-          )}
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default TrackCollab;
