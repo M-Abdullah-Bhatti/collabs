f77c36367f14b21934acdc191b5b405ab4c76143
ShayanJamil519 <shayanjamil500@gmail.com>
2023-10-07 17:13:54 +0500

    Header Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Collab from "./Pages/Collab";
 import Profile from "./Pages/Profile/Profile";
+import Header from "./Components/Shared/Header/Header";
 
 function App() {
   return (
     <div className="">
+      <Header />
       <Routes>
         <Route path="/collab" element={<Collab />} />
         <Route path="/" element={<Profile />} />
