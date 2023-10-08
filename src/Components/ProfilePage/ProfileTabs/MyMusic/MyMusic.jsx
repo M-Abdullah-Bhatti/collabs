87875609440039d0267e48b3470c000dba670960
ShayanJamil519 <shayanjamil500@gmail.com>
2023-10-08 14:47:53 +0500

    Height Issue Resolved

--- src/Components/ProfilePage/ProfileTabs/MyMusic/MyMusic.jsx
+++ src/Components/ProfilePage/ProfileTabs/MyMusic/MyMusic.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import MyCollabMusicPlayer from '../MyCollabs/MyCollabMusicPlayer/MyCollabMusicPlayer'
+import React from "react";
+import MyCollabMusicPlayer from "../MyCollabs/MyCollabMusicPlayer/MyCollabMusicPlayer";
 
 const MyMusic = () => {
   return (
-    <div style={{marginTop: '30px'}}>
-        <MyCollabMusicPlayer status={0} />
-        <div style={{ backgroundColor: '#fff', padding: '20px'}}></div>
+    <div style={{ marginTop: "30px" }}>
+      <MyCollabMusicPlayer status={0} />
+      {/* <div style={{ backgroundColor: '#fff', padding: '20px'}}></div> */}
     </div>
-  )
-}
+  );
+};
 
-export default MyMusic
+export default MyMusic;
